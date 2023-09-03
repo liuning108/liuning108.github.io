c00d59d2330c22018528855ce5afb1cacdbe132a
Liu ning <122273014@qq.com>
2023-09-04 06:50:00 +0800

    add canvas_02

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,8 @@
 
 "use client"
 import {BsFillMoonStarsFill, BsFillSunFill} from 'react-icons/bs'
+import  Canva01 from '@/app/canvas_js/01/page'
+import  Canva02 from '@/app/canvas_js/02/page'
 
 import {
     AiFillAccountBook,
@@ -13,17 +15,8 @@ import {
 } from 'react-icons/ai'
 import Image from "next/image";
 import deved from '../../../public/dev-ed-wave.png'
-import design from '../../../public/design.png'
-import code from '../../../public/code.png'
-import consulting from '../../../public/consulting.png'
-
-import web1 from '../../../public/web1.png'
-import web2 from '../../../public/web2.png'
-import web3 from '../../../public/web3.png'
-import web4 from '../../../public/web4.png'
-import web5 from '../../../public/web5.png'
-import web6 from '../../../public/web6.png'
 import {useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
     const [darkMode,setDarkMode] = useState(true);
@@ -40,9 +33,9 @@ export default function Home() {
                  <nav className={"py-10 mb-12 flex justify-around"}>
                      <h1 className={"text-xl font-burtons"}>Liu-Ning . Dev</h1>
                      <ul className={"flex items-center"}>
-                         <li>
-                             {button}
-                         </li>
+                         {/*<li>*/}
+                         {/*    {button}*/}
+                         {/*</li>*/}
                          <li>
                              {/*<a  className={"bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 font-burtons"} href={"#"}>经历</a>*/}
                          </li>
@@ -69,140 +62,55 @@ export default function Home() {
 
              <section>
                  <div className={"dark:text-white"}>
-                     <h3 className={"text-3xl py-1"}>Services I offer</h3>
-                     <p className={"text-md py-2 leading-8 text-gray-80"}>
-                         Since the beginning  of my journey as freel developer , I've done remote work for
-                         <span className="text-teal-500"> agencies </span>
-                         consulted for <span className="text-teal-500">startups </span>
-                         and collaborated with talanted people to create digital products
-                         for both business and consumer use.
+                     <h3 className={"text-3xl py-1"}>提供的服务</h3>
+                     <p className={"text-xl py-2 leading-8 text-gray-80"}>
+                         作为一名开发者，我曾在多家<span className="text-teal-500">机构</span>从事远程工作，为<span className="text-teal-500">初创企业</span>提供咨询，并与各种人才合作，为企业和消费者开发数字产品。
 
                      </p>
-                     <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-                         I offer from a wide range of services, including brand design,
-                         programming and teaching.
+                     <p className="text-xl py-2 leading-8 text-gray-800 dark:text-gray-200">
+                         我提供广泛的服务，包括品牌设计、编程和教学。
                      </p>
                  </div>
 
-                 <div className="lg:flex gap-10">
-                     <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-                         <Image src={design} alt={""} width={100} height={100}  />
-                         <h3 className="text-lg font-medium pt-8 pb-2  ">
-                             Beautiful Designs
-                         </h3>
-                         <p className="py-2">
-                             Creating elegant designs suited for your needs following core
-                             design theory.
-                         </p>
-                         <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-                         <p className="text-gray-800 py-1">Photoshop</p>
-                         <p className="text-gray-800 py-1">Illustrator</p>
-                         <p className="text-gray-800 py-1">Figma</p>
-                         <p className="text-gray-800 py-1">Indesign</p>
-                     </div>
-                     <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-                         <Image src={code} width={100} height={100} alt={""} />
-                         <h3 className="text-lg font-medium pt-8 pb-2 ">
-                             Code your dream project
-                         </h3>
-                         <p className="py-2">
-                             Do you have an idea for your next great website? Let's make it a
-                             reality.
-                         </p>
-                         <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-                         <p className="text-gray-800 py-1">Photoshop</p>
-                         <p className="text-gray-800 py-1">Illustrator</p>
-                         <p className="text-gray-800 py-1">Figma</p>
-                         <p className="text-gray-800 py-1">Indesign</p>
-                     </div>
-                     <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-                         <Image src={consulting} width={100} height={100} alt={""}/>
-                         <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-                         <p className="py-2">
-                             Are you interested in feedback for your current project? I can
-                             give you tips and tricks to level it up.
-                         </p>
-                         <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-                         <p className="text-gray-800 py-1">Photoshop</p>
-                         <p className="text-gray-800 py-1">Illustrator</p>
-                         <p className="text-gray-800 py-1">Figma</p>
-                         <p className="text-gray-800 py-1">Indesign</p>
-                     </div>
-                 </div>
+
 
              </section>
              <section>
                  <section className="py-10">
                      <div>
-                         <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-                         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-                             Since the beginning of my journey as a freelance designer and
-                             developer, I've done remote work for
-                             <span className="text-teal-500"> agencies </span>
-                             consulted for <span className="text-teal-500">startups </span>
-                             and collaborated with talanted people to create digital products
-                             for both business and consumer use.
-                         </p>
-                         <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-                             I offer from a wide range of services, including brand design,
-                             programming and teaching.
-                         </p>
+                         <h3 className="text-3xl py-1 dark:text-white ">练习簿</h3>
+
                      </div>
                      <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-                         <div className="basis-1/3 flex-1  ">
-                             <Image
-                                 className="rounded-lg object-cover"
-
-                                 layout="responsive"
-                                 src={web1}
-                                 alt={""}
-                             />
+                         <div className="basis-1/3 flex-1 text-black bg-blue-500 max-h-[490px] overflow-hidden rounded-xl shadow-lg">
+                             <Link href={"/canvas_js/02"} target={"_blank"}>
+                                 <div className={"scale-[0.6]  origin-top-left  "}>
+                                     <Canva02></Canva02>
+                                 </div>
+                             </Link>
                          </div>
-                         <div className="basis-1/3 flex-1">
-                             <Image
-                                 className="rounded-lg object-cover"
 
-                                 alt={""}
-                                 layout="responsive"
-                                 src={web2}
-                             />
+                         <div className="basis-1/3 flex-1 text-black bg-white max-h-[490px] overflow-hidden ">
+                             <Link href={"/canvas_js/01"} target={"_blank"}>
+                             <div className={"scale-[0.6]  origin-top-left  cursor-pointer "}>
+                                 <Canva01></Canva01>
+                             </div>
+                             </Link>
                          </div>
-                         <div className="basis-1/3 flex-1">
-                             <Image
-                                 className="rounded-lg object-cover"
 
-                                 alt={""}
-                                 layout="responsive"
-                                 src={web3}
-                             />
-                         </div>
-                         <div className="basis-1/3 flex-1">
-                             <Image
-                                 className="rounded-lg object-cover"
 
-                                 alt={""}
-                                 layout="responsive"
-                                 src={web4}
-                             />
-                         </div>
-                         <div className="basis-1/3 flex-1">
-                             <Image
-                                 className="rounded-lg object-cover"
 
-                                 alt={""}
-                                 layout="responsive"
-                                 src={web5}
-                             />
-                         </div>
-                         <div className="basis-1/3 flex-1">
-                             <Image
-                                 className="rounded-lg object-cover"
 
-                                 layout="responsive"
-                                 src={web6}
-                                 alt={""}
-                             />
-                         </div>
+                         {/*div<div className="basis-1/3 flex-1 max-w-[50%]">*/}
+                         {/*    <Image*/}
+                         {/*        className="rounded-lg object-cover"*/}
+
+                         {/*        layout="responsive"*/}
+                         {/*        src={web1}*/}
+                         {/*        alt={""}*/}
+                         {/*    />*/}
+                         {/*</div>*/}
+
                      </div>
                  </section>
              </section>
