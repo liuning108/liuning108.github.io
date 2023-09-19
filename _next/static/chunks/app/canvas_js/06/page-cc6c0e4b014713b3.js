2b9b2a2cf2e78b5ecf055bb5ca82c72772fdd616
Liu ning <122273014@qq.com>
2023-09-20 05:56:25 +0800

    Deploy Next.js to gh-pages

---
+++ _next/static/chunks/app/canvas_js/06/page-cc6c0e4b014713b3.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[263],{84:function(e,r,t){Promise.resolve().then(t.bind(t,2282))},2282:function(e,r,t){"use strict";t.r(r);var n=t(7437);r.default=e=>(0,n.jsx)("main",{className:"absolute w-full h-full bg-gray-100"})},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function l(e,r,t){var n,l={},_=null,a=null;for(n in void 0!==t&&(_=""+t),void 0!==r.key&&(_=""+r.key),void 0!==r.ref&&(a=r.ref),r)s.call(r,n)&&!u.hasOwnProperty(n)&&(l[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===l[n]&&(l[n]=r[n]);return{$$typeof:o,type:e,key:_,ref:a,props:l,_owner:f.current}}r.jsx=l,r.jsxs=l},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=84)}),_N_E=e.O()}]);
