a272cd043329aab49dfc573b734348eb772d5d64
Liu ning <122273014@qq.com>
2023-09-26 06:18:09 +0800

    Deploy Next.js to gh-pages

---
+++ _next/static/chunks/app/canvas_js/02/page-1a39a96397926dfd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9647,8421,6846,2348,7091,8895,8259,1299],{3521:function(){}},function(n){n.O(0,[2971,596,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
