b64b6e86b2c1728f2a5b2a4ec0aff962b3b4b9ef
Liu ning <122273014@qq.com>
2024-05-12 06:17:38 +0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/chunks/app/game/page-2c586461c19f055b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8421,5727,6846,9647,1299,7091,2348,8895,8259],{3521:function(){}},function(n){n.O(0,[2971,596,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
